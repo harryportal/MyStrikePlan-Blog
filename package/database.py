8d60054d0b16b027333cf60e32d5e348903691fa
Kuye Damilola <37317864+harryportal@users.noreply.github.com>
2022-03-23 16:30:52 +0100

    Update database.py

--- package/database.py
+++ package/database.py
@@ -13,10 +13,10 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(20), nullable=False)
-    lastname = db.Column(db.String(20), nullable=False)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(20), unique=True, nullable=False)
+    firstname = db.Column(db.String, nullable=False)
+    lastname = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
     image = db.Column(db.String, nullable=False, default='default.jpg')
     password = db.Column(db.String, nullable=False)
     confirm = db.Column(db.Boolean, default=False)
@@ -45,7 +45,7 @@ class Post(db.Model):
     __tablename__ = 'Post'
     __searchable__ = ['title', 'content']
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
