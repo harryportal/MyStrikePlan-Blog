7c8a992b9d1a5535040f4719ea7a091f20b37028
Kuye Damilola <37317864+harryportal@users.noreply.github.com>
2022-03-23 12:51:48 +0100

    Update config.py

--- package/config.py
+++ package/config.py
@@ -6,6 +6,6 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
