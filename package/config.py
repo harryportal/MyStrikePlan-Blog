b9c62c6c34049403977519fa81455d5bbc8058eb
harry <dammykuye@gmail.com>
2022-05-03 03:36:56 +0100

    db fix

--- package/config.py
+++ package/config.py
@@ -6,7 +6,8 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SECRET_KEY = 'db176a3a11534a517319'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = "postgres://yzrvalgigyegtz:f1b235c6cae9f1cd02b14eb2b52a832ae4ac876619e73df18ea75b93e3f14bbd@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d4psa2co7p3ebq"
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
           SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
