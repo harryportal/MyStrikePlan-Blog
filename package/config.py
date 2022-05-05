cd51ce759d1f0e173a6e70b195f872a1fd6b4131
harry <dammykuye@gmail.com>
2022-05-05 10:24:01 +0100

    FIXED BOLD TEXT

--- package/config.py
+++ package/config.py
@@ -1,15 +1,15 @@
 import os
-import re
+
+
+
 class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = "postgres://yzrvalgigyegtz:f1b235c6cae9f1cd02b14eb2b52a832ae4ac876619e73df18ea75b93e3f14bbd@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d4psa2co7p3ebq"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-          SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-
-
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
